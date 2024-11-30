1ee5090fe29879451b2c3a09d85799bc24e7629a
Arnaud <heyarnaudle@gmail.com>
2024-11-30 15:15:58 +0100

    Initial commit

---
+++ src/main/java/fr/ale/stock_api/GestionDesStocksApplication.java
@@ -0,0 +1,13 @@
+package fr.ale.stock_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesStocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ale/stock_api/GestionDesStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ale.stock_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesStocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
