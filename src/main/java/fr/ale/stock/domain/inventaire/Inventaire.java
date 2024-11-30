1673b22679c1939823bd06a7afc5679468e2768e
Arnaud <heyarnaudle@gmail.com>
2024-11-30 16:52:17 +0100

    feat: ajoute objets "domain" Inventaire et Produit

---
+++ src/main/java/fr/ale/stock/domain/inventaire/Inventaire.java
@@ -0,0 +1,6 @@
+package fr.ale.stock.domain.inventaire;
+
+import java.util.List;
+
+public record Inventaire(List<Produit> produits) {
+}
---
+++ src/main/java/fr/ale/stock/domain/inventaire/Produit.java
@@ -0,0 +1,4 @@
+package fr.ale.stock.domain.inventaire;
+
+public record Produit(String name) {
+}
